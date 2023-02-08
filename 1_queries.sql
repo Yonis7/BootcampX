67dbfff0f25ee0c869c6e95aae36893c867557dc
Yonis7 <yonisaden7@gmail.com>
2023-02-08 16:19:23 +0000

    BootcampX Queries II - JOIN

--- 1_queries.sql
+++ 1_queries.sql
@@ -24,4 +24,18 @@
 -- FROM students
 -- WHERE github IS NULL AND end_date IS NOT NULL;
 
-SELECT count(*) FROM assignment_submissions;
+-- SELECT count(*) FROM assignment_submissions;
+-- SELECT students.name as student_name, email, cohorts.name as cohort_name
+-- FROM students JOIN cohorts ON cohort_id = cohorts.id;
+
+-- SELECT students.name as student_name, email, cohorts.name as cohort_name
+-- FROM students INNER JOIN cohorts ON cohorts.id = cohort_id;
+
+-- SELECT students.name as student_name, email, cohorts.name as cohort_name
+-- FROM students RIGHT JOIN cohorts ON cohorts.id = cohort_id;
+
+SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
